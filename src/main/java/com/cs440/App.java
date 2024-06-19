f28ba83d574ccb69671224d9dc8cc0b79b456b8f
Elian <elian.ddb@outlook.com>
2024-06-19 10:12:02 -0400

    add wait for {Enter}

--- src/main/java/com/cs440/App.java
+++ src/main/java/com/cs440/App.java
@@ -1,8 +1,12 @@
 package com.cs440;
 
+import java.io.IOException;
+
 public class App {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Ship ship = new Ship(40, 40);
         System.out.println(ship);
+        
+        System.in.read();
     }
 }
