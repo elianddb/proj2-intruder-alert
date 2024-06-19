92ab312ddd3721d6ec0ed586f8a9dfd2325acbe2
Elian <elian.ddb@outlook.com>
2024-06-19 11:24:23 -0400

    add feedback when ready to exit

--- src/main/java/org/cs440/App.java
+++ src/main/java/org/cs440/App.java
@@ -8,7 +8,8 @@ public class App {
     public static void main(String[] args) throws IOException {
         Ship ship = new Ship(40, 40);
         System.out.println(ship);
-
+        
+        System.out.println("Press Enter to exit...");
         System.in.read();
     }
 }
