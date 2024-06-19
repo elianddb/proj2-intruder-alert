e48924ff96fba33e133f2ecb1b8a7481fac571bc
Elian <elian.ddb@outlook.com>
2024-06-19 00:12:07 -0400

    rm extra space in main()

--- src/main/java/com/cs440/App.java
+++ src/main/java/com/cs440/App.java
@@ -1,7 +1,7 @@
 package com.cs440;
 
 public class App {
-    public static void main( String[] args ) {
+    public static void main(String[] args) {
         System.out.println("Hello World!");
     }
 }
