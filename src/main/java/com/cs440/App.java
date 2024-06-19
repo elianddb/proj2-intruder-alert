680d244dbe7bd9c0936bf740b0062d34b3f6b2ef
Elian <elian.ddb@outlook.com>
2024-06-19 10:21:47 -0400

    add Ship import

--- src/main/java/com/cs440/App.java
+++ src/main/java/com/cs440/App.java
@@ -2,11 +2,13 @@ package com.cs440;
 
 import java.io.IOException;
 
+import com.cs440.ship.Ship;
+
 public class App {
     public static void main(String[] args) throws IOException {
         Ship ship = new Ship(40, 40);
         System.out.println(ship);
-        
+
         System.in.read();
     }
 }
