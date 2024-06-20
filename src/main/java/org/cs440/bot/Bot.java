74bff989224758be7d35e4cab752fcb59dfaa5d4
Elian <elian.ddb@outlook.com>
2024-06-20 15:27:24 -0400

    mv Bot.java to entity pckg

--- src/main/java/org/cs440/bot/Bot.java
+++
@@ -1,5 +0,0 @@
-package org.cs440.bot;
-
-public abstract class Bot {
-    
-}
---
+++ src/main/java/org/cs440/entity/Bot.java
@@ -0,0 +1,9 @@
+package org.cs440.entity;
+
+import org.cs440.ship.Ship;
+
+public class Bot extends Entity {
+    public Bot(Ship ship, char identifier) {
+        super(ship, identifier);
+    }
+}
