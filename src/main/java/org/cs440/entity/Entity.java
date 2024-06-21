f37e4f6c93c7f75b11687b2e3d4918773e2882a8
Elian <elian.ddb@outlook.com>
2024-06-21 17:42:47 -0400

    amend Entity.java rm move()

--- src/main/java/org/cs440/entity/Entity.java
+++ src/main/java/org/cs440/entity/Entity.java
@@ -4,21 +4,6 @@ import org.cs440.ship.Ship;
 import org.cs440.ship.Tile.Location;
 
 public abstract class Entity {
-    public enum Direction {
-        UP(0, 1),
-        DOWN(0, -1),
-        LEFT(-1, 0),
-        RIGHT(1, 0);
-
-        public final int dx;
-        public final int dy;
-
-        private Direction(int dx, int dy) {
-            this.dx = dx;
-            this.dy = dy;
-        }
-    }
-
     protected Ship ship;
     protected char identifier;
     protected Location location;
@@ -32,14 +17,5 @@ public abstract class Entity {
         this.location = ship.requestOpen(); // Returns new Location object
         ship.setTile(location, Ship.OCCUPIED);
         this.identifier = identifier;
-    }
-
-    public void move(Direction direction) {
-        Location target = new Location(location.x() + direction.dx, location.y() + direction.dy);
-        if (!ship.withinBounds(target)) {
-            return;
-        }
-
-        ship.requestTransfer(location, target);
     }
 }
