cfa4fa06bded9563b418ee771fce5b7ca13197c7
Elian <elian.ddb@outlook.com>
2024-06-21 17:44:22 -0400

    add extension StochasticMouse -> Entity

--- src/main/java/org/cs440/entity/StochasticMouse.java
+++ src/main/java/org/cs440/entity/StochasticMouse.java
@@ -6,12 +6,4 @@ public class StochasticMouse extends Entity {
     public StochasticMouse(Ship ship, char identifier) {
         super(ship, identifier);
     }
-
-    @Override
-    public void move(Direction direction) {
-        // StochasticMouse moves in a random direction
-        Direction[] directions = Direction.values();
-        int randomIndex = (int) (Math.random() * directions.length);
-        super.move(directions[randomIndex]);
-    }
 }
