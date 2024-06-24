1b4907d0374279c11c2de863909597f3a08eddbb
Elian <elian.ddb@outlook.com>
2024-06-24 16:53:39 -0400

    add StochasticMouse Action interface

--- src/main/java/org/cs440/agent/StochasticMouse.java
+++ src/main/java/org/cs440/agent/StochasticMouse.java
@@ -1,12 +1,14 @@
 package org.cs440.agent;
 
+import org.cs440.App;
+import org.cs440.agent.Agent.Action;
 import org.cs440.agent.Agent.Movement;
 import org.cs440.ship.Ship;
 import org.cs440.ship.Tile;
 import org.cs440.ship.Tile.Status;
 
 // TODO Implement Movement & Action interface
-public class StochasticMouse extends Agent implements Movement {
+public class StochasticMouse extends Agent implements Movement, Action {
     public StochasticMouse(char identifier) {
         super(identifier);
     }
@@ -30,5 +32,36 @@ public class StochasticMouse extends Agent implements Movement {
         location = destination.location();
 
         ship.setTile(location, Ship.OCCUPIED);
+    }
+
+    @Override
+    public void perform() {
+        // Randomly move in a direction
+        int direction = (int) (Math.random() * 4);
+        App.logger.debug("StochasticMouse " + identifier + " moving in direction " + direction);
+        switch (direction) {
+            case 0:
+                move(Direction.UP);
+                break;
+            case 1:
+                move(Direction.DOWN);
+                break;
+            case 2:
+                move(Direction.LEFT);
+                break;
+            case 3:
+                move(Direction.RIGHT);
+                break;
+        }
+    }
+
+    @Override
+    public boolean closed() {
+        return COEXTENSIVE;
+    }
+
+    @Override
+    public String toString() {
+        return "StochasticMouse [identifier=" + identifier + ", location=" + location + "]";
     }
 }
