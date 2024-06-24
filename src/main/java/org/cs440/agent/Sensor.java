b72d1f752c5ec9fd946033971220b4b8d5a7056e
elianddb <elian.ddb@outlook.com>
2024-06-23 20:11:29 -0400

    amend Sensor

--- src/main/java/org/cs440/agent/Sensor.java
+++ src/main/java/org/cs440/agent/Sensor.java
@@ -1,5 +1,13 @@
 package org.cs440.agent;
 
 public class Sensor {
+    protected Agent user;
+    protected Agent target;
 
+    public Sensor(Agent user, Agent target) {
+        this.user = user;
+        this.target = target;
+    }
+
+    
 }
