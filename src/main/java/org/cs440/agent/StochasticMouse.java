05a247ae8955eaeaed508c7844ca8f7414823ee5
elianddb <elian.ddb@outlook.com>
2024-06-23 21:37:09 -0400

    amend StochasticMouse TODO

--- src/main/java/org/cs440/agent/StochasticMouse.java
+++ src/main/java/org/cs440/agent/StochasticMouse.java
@@ -1,6 +1,6 @@
 package org.cs440.agent;
 
-// TODO Implement Movement interface
+// TODO Implement Movement & Action interface
 public class StochasticMouse extends Agent {
     public StochasticMouse(char identifier) {
         super(identifier);
