ce88d1be77f620a842e56cc003a519744e282242
Elian <elian.ddb@outlook.com>
2024-06-24 16:13:18 -0400

    add conditional to read init state logs

--- src/main/java/org/cs440/App.java
+++ src/main/java/org/cs440/App.java
@@ -2,15 +2,16 @@ package org.cs440;
 
 import java.io.IOException;
 
+import org.cs440.Log.Level;
 import org.cs440.agent.Bot;
 import org.cs440.agent.StationaryMouse;
 import org.cs440.ship.Ship;
 
 public class App {
-    public static final Log logger = new Log("App");
+    public static final Log logger = new Log("Logger");
 
-    public static void main(String[] args) throws IOException {
-        logger.setLevel(Log.Level.DEBUG);
+    public static void main(String[] args) throws IOException, InterruptedException {
+        logger.setLevel(Level.DEBUG);
         
         Ship ship = new Ship(40, 40);
         StationaryMouse mouse = new StationaryMouse('M');
@@ -19,6 +20,12 @@ public class App {
         Simulation simulation = new Simulation(ship);
         simulation.addAgent(bot);
         simulation.addAgent(mouse);
+        
+        if (logger.is(Level.DEBUG)) { // Delay to read initial state logs
+            logger.debug("Debugging is enabled...");
+            Thread.sleep(3000);
+        }
+
         simulation.run(100); // Exclude delay to run without drawing frames
 
         System.out.println("Press Enter to exit...");
